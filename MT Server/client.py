c05b1c71319286697abd940a7d5fcac4aeeb616e
Ilay Levy <ilayl9991@gmail.com>
2020-11-05 15:55:52 +0200

    client functen get and send

--- MT Server/client.py
+++ MT Server/client.py
@@ -1,17 +1,24 @@
 import socket
 
+def get_n_print():
+    Response = ClientSocket.recv(512)
+    print(Response.decode())
+def enter_n_send():
+    inp = Input()
+    ClientSocket.send(str.encode(inp))
 ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 10000
-
-print('Waiting for connection')
+print('try to connect..')
 try:
     ClientSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
-Response = ClientSocket.recv(1024)
-print(Response.decode())
+get_n_print()
+enter_n_send()
+get_n_print()
+
 while True:
     Input = input('Say Something (to disconnect write "quit"): ')
     ClientSocket.send(str.encode(Input))
