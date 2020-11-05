c522178a9ea3603a909b2bc4a85e82f4cd1ff1e7
Ilay Levy <ilayl9991@gmail.com>
2020-11-05 20:36:41 +0200

    Client class

--- MT Server/client.py
+++ MT Server/client.py
@@ -1,30 +1,43 @@
 import socket
+from _thread import *
 
-def get_n_print():
-    Response = ClientSocket.recv(512)
-    print(Response.decode())
-def enter_n_send():
-    inp = Input()
-    ClientSocket.send(str.encode(inp))
-ClientSocket = socket.socket()
 host = '127.0.0.1'
 port = 10000
-print('try to connect..')
-try:
-    ClientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
 
-get_n_print()
-enter_n_send()
-get_n_print()
+class Client:
+    def __init__(self):
+        self.sc = socket.socket()
+        print('try to connect..')
+        try:
+            self.sc.connect((host, port))
+        except socket.error as e:
+            print(str(e))
+            exit(1)
 
-while True:
-    Input = input('Say Something (to disconnect write "quit"): ')
-    ClientSocket.send(str.encode(Input))
-    if Input.lower() == 'quit':
-        break
-    Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
+    def get_n_print(self):
+        Response = self.cs.recv(512)
+        print(Response.decode())
+        
+    def enter_n_send(self):
+        inp = Input()
+        self.cs.send(str.encode(inp))
 
-ClientSocket.close()
+
+
+
+
+if __name__ == "__main__":
+    clnt = Client()
+    clnt.get_n_print()
+    clnt.enter_n_send()
+    clnt.get_n_print()
+
+    while True:
+        Input = input('Say Something (to disconnect write "quit"): ')
+        ClientSocket.send(str.encode(Input))
+        if Input.lower() == 'quit':
+            break
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+
+    ClientSocket.close()
