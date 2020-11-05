99d67bd455e14e0892a9f81f1802383d73e004f9
Ilay Levy <ilayl9991@gmail.com>
2020-11-05 21:30:33 +0200

    fix exeption in server thread

--- MT Server/server.py
+++ MT Server/server.py
@@ -35,36 +35,42 @@ class Server:
 
 
     def threaded_client(self, sc, addr):
-        sc.send(str.encode('Welcome to the Server\nsend ur name to chat (16 leters max)\n'))
-        name = sc.recv(16).decode()
-        sc.sendall(str.encode("Hi " + name + '\nu can chat now (enter q for exit)'))
-        self.client_lock.acquire()
-        self.cliets_list += (sc,addr,name)
-        self.Thread_count += 1
-        self.client_lock.release()
-        
-        while True:
-            try:
-                data = sc.recv(512).decode()
-                msg = "->" + name + ': ' + data
-                if not data or data == 'q':
+        try:
+            sc.send(str.encode('Welcome to the Server\nsend ur name to chat (16 leters max)\n'))
+            name = sc.recv(16).decode()
+            sc.sendall(str.encode("Hi " + name + '\nu can chat now (enter q for exit)'))
+            is_connected = True
+        except :
+            is_connected = False
+
+        if is_connected:
+            self.client_lock.acquire()
+            self.cliets_list += (sc,addr,name)
+            self.Thread_count += 1
+            self.client_lock.release()
+            
+            while True:
+                try:
+                    data = sc.recv(512).decode()
+                    if not data or data == 'q':
+                        break
+                    msg = "->" + name + ': ' + data
+                    self.client_lock.acquire()
+                    for cln in self.cliets_list:
+                        if sc not in cln:
+                            self.sock_lock.acquire()
+                            cln[0].sendall(msg.encode())
+                            self.sock_lock.release()
+                    self.client_lock.release()
+                except:
+                    print(name + 'client disconected')
                     break
-                self.client_lock.acquire()
-                for cln in self.cliets_list:
-                    if sc not in cln:
-                        self.sock_lock.acquire()
-                        cln[0].sendall(msg.encode())
-                        self.sock_lock.release()
-                self.client_lock.release()
-            except:
-                print(name + 'client disconected')
-                break
-        #print("\n", address[0] + ':' + str(address[1]), " disconnected", "\n")
-        self.client_lock.acquire()
-        self.Thread_count -= 1
-        self.cliets_list.remove(sc,addr,name)
-        self.client_lock.release()
-        sc.close()
+            #print("\n", address[0] + ':' + str(address[1]), " disconnected", "\n")
+            self.client_lock.acquire()
+            self.Thread_count -= 1
+            self.cliets_list.remove(sc,addr,name)
+            self.client_lock.release()
+            sc.close()
 
 
 
