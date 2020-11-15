78e7ae6ab9137e32730fc5f1a0c0bdc23b96dd71
ori lahav <oril20503@gmail.com>
2020-11-15 18:53:04 +0200

    added function documenting

--- DB_helper.py
+++ DB_helper.py
@@ -1,4 +1,10 @@
 def catch_exception_get_db(schema, error):
+    """
+    the function manage all the schemas to the firebase database that related to get data from the database
+    :param schema: the command that gives the data from the database
+    :param error: the error that the schema can cuz
+    :return: the data from the database
+    """
     values = False
     try:
         values = schema
@@ -8,6 +14,12 @@ def catch_exception_get_db(schema, error):
 
 
 def catch_exception_put_db(schema, error):
+    """
+    the function manage all the schemas to the firebase database that related to put data in the database
+    :param schema: the command that put the data in the database
+    :param error: the error that the schema can cuz
+    :return: if the schema worked good
+    """
     try:
         schema
     except:
@@ -17,6 +29,11 @@ def catch_exception_put_db(schema, error):
 
 
 def get_free_ip(db):
+    """
+    the function give the first free to use ip from the database
+    :param db: the database with the ips
+    :return: the ip
+    """
     ips = catch_exception_get_db(db.child('IPS').order_by_child('used').limit_to_first(1).get().val(),
                                  'ERROR: cant get ip...')
     ip = ips.popitem()
@@ -27,6 +44,13 @@ def get_free_ip(db):
 
 
 def delete_user(username, token, email):
+    """
+    the function verify the delete of the user from the database.
+    :param username: the username
+    :param token: the token of the user
+    :param email: the email of the user
+    :return: None
+    """
     sure = input("to verify the delete of the account type the username <" + username + ">: ")
     if sure == username:
         del_user(token, email)
@@ -34,7 +58,13 @@ def delete_user(username, token, email):
         print('i guess you not that sure...')
 
 
-def del_user(db,auth, token, email):
+def del_user(token, email):
+    """
+    the function handle all the user delete
+    :param token: the user token
+    :param email: the email of the user
+    :return: None
+    """
     try:
         auth.delete_user_account(token)
         flag = True
@@ -44,7 +74,7 @@ def del_user(db,auth, token, email):
 
     if flag is True:
         stats = catch_exception_get_db(db.child('Users').order_by_child('email').equal_to(email).get().val(),
-                                   'ERROR: cant get the stats')
+                                       'ERROR: cant get the stats')
         stat = stats.popitem()
         del_user_info = catch_exception_put_db(db.child('Users').child(stat[0]).set(None), 'ERROR: cant delete user')
         ips = catch_exception_get_db(
@@ -53,6 +83,3 @@ def del_user(db,auth, token, email):
         ip = ips.popitem()
         catch_exception_put_db(db.child('IPS').child(ip[0]).child('used').set(False),
                                'ERROR: cant change ip to not used')
-
-
-    
--- projDB.py
+++ projDB.py
@@ -4,23 +4,26 @@ import pyrebase
 HOST = '127.0.0.1'
 PORT = 4000
 firebaseConfig = {'apiKey': "AIzaSyAsWlvXK-lblE2C9QWt8HNwKKCO6GsB26E",
-    'authDomain': "subloader-98331.firebaseapp.com",
-    'databaseURL': "https://subloader-98331.firebaseio.com",
-    'projectId': "subloader-98331",
-    'storageBucket': "subloader-98331.appspot.com",
-    'messagingSenderId': "763871684411",
-    'appId': "1:763871684411:web:6f8cca8650dfc178cb391e",
-    'measurementId': "G-2WBR7EQDYC"}
+                  'authDomain': "subloader-98331.firebaseapp.com",
+                  'databaseURL': "https://subloader-98331.firebaseio.com",
+                  'projectId': "subloader-98331",
+                  'storageBucket': "subloader-98331.appspot.com",
+                  'messagingSenderId': "763871684411",
+                  'appId': "1:763871684411:web:6f8cca8650dfc178cb391e",
+                  'measurementId': "G-2WBR7EQDYC"}
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 database = firebase.database()
 authentication = firebase.auth()
+
+
 # storage = firebase.storage()
 
-#protocols:
-#singup- 01email!name!password!conf password #01s\femail\err msg!ip\none
-#login-  02name\email!password               #02s\f(username!ip)\err
-        
+# protocols:
+# singup- 01email!name!password!conf password #01s\femail\err msg!ip\none
+# login-  02name\email!password               #02s\f(username!ip)\err
+
+
 class Singup:
     def __init__(self, email, usrn, pw, conf):
         self.username = usrn
@@ -28,55 +31,60 @@ class Singup:
         self.email = email
         self.conf_pw = conf
 
+
 class Login:
     def __init__(self, email, pw):
         self.password = pw
         self.email = email
 
+
 class Google_DB:
-    def __init__(self,db, auth):
+    def __init__(self, db, auth):
         self.db = db
         self.auth = auth
 
-    def singup(self, s_up = Singup):
+    def singup(self, s_up=Singup):
         if s_up.password == s_up.conf_pw:
             try:
-                self.auth.create_user_with_email_and_password(s_up.email, s_up.password)  # Sign up with email and password
+                self.auth.create_user_with_email_and_password(s_up.email,
+                                                              s_up.password)  # Sign up with email and password
                 print("User successfully created!")
-                #flag = True
             except Exception as e:
                 print("ERROR: Wrong Email or Password input!")
                 return False, str(e)
-                #flag = False
 
-        #if flag:
             ip_id, ip = get_free_ip(self.db)
             user_info = catch_exception_put_db(
                 self.db.child("Users").child(s_up.email.split('@')[0]).set({'username': s_up.username, 'ip': ip,
-                                                                  'email': s_up.email}), 'ERROR: cant add new user')
-            update_ip = catch_exception_put_db(self.db.child("IPS").child(ip_id).update({'used': True}), 'ERROR: cant change parameter')
+                                                                            'email': s_up.email}),
+                'ERROR: cant add new user')
+            update_ip = catch_exception_put_db(self.db.child("IPS").child(ip_id).update({'used': True}),
+                                               'ERROR: cant change parameter')
             return True, ip
         else:
             return False, "passwords not match!"
-    
-    def login(self, l_in = Login):
-        #login auth section
+
+    def login(self, l_in=Login):
+        # login auth section
         try:
-            token = self.auth.sign_in_with_email_and_password(l_in.email, l_in.password)  # Sign up with email and password
+            token = self.auth.sign_in_with_email_and_password(l_in.email,
+                                                              l_in.password)  # Sign up with email and password
             print("User successfully logged in!")
         except Exception as e:
             print("ERROR: Wrong Email or Password input!")
-            return False, str(e) 
-        #get user data section
+            return False, str(e)
+            # get user data section
         stats = catch_exception_get_db(self.db.child('Users').order_by_child('email').equal_to(l_in.email).get().val(),
-                                   'ERROR: cant get the stats')
+                                       'ERROR: cant get the stats')
         stat = stats.popitem()
-        return True, (stat[0], stat[1]['ip'], token['idToken'])# if the action succed and the user info (name,ip,token)
+        return True, (
+            stat[0], stat[1]['ip'], token['idToken'])  # if the action succeed and the user info (name,ip,token)
+
 
 if __name__ == '__main__':
-    #tester
+    # tester
     ipsresulte = get_free_ip(database)
     print(ipsresulte)
-    gdb = Google_DB(database,authentication)
+    gdb = Google_DB(database, authentication)
     loginres = gdb.singup(Singup("ilay@gmail.com", 'gg', 'ilay120', 'ilay120'))
     print(loginres)
