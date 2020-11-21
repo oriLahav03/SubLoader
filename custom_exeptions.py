e3e9884d9af4b9244e584ef9e3b6ac0969bb79ed
Ilay Levy <ilayl9991@gmail.com>
2020-11-22 00:22:14 +0200

    room not exist& name taken error

--- custom_exeptions.py
+++ custom_exeptions.py
@@ -25,3 +25,23 @@ class update_table_err(Exception):
 
     def __str__(self):
         return 'error update ' + self.table + " " + self.err
+
+class room_not_exist(Exception):
+    """
+    room not exist expetion
+    """
+    def __init__(self, room_name, msg=''):
+        super().__init__(msg)
+        self.room = room_name
+    
+    def __str__(self):
+        return 'room \'' + self.room + "' not exist "
+
+class name_taken(Exceptions):
+    def __init__(self, name, zone):
+        super(name_taken, self).__init__(name)
+        self.name = name         
+        self.zone = zone
+
+    def __str__(self):
+        return self.name + ' already exists in ' + self.zone
