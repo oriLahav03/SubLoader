d2196e7a8394b41d5de2e2f4e4ff12086f99fcde
Ilay Levy <ilayl9991@gmail.com>
2020-11-24 01:32:55 +0200

    update classes

--- vpn_manager.py
+++ vpn_manager.py
@@ -1,20 +1,23 @@
+class Room():
+    """
+    manage room info
+    users: just virtual ip clients that in the room
+    """
+    def __init__(self, name, users, sets : dict):
+        self.room_name = name
+        self.users = users
+        self.sets = sets
+
+
 class Client():
     """
     manage client info
     """
-    def __init__(self, sc : socket.socket, addr, name, email, vir_ip, rooms):
+    def __init__(self, sc : socket.socket, addr, name, email, vir_ip, rooms: list[Room]):
         self.sc = sc
         self. addr = addr
         self.rooms = rooms
         self.name = name
         self.email = email
         self.vir_ip = vir_ip
-
-class Room():
-    """
-    manage room info
-    users: just virtual ip clients that in the room
-    """
-    def __init__(self, name, users):
-        self.room_name = name
-        self.users = users
+        
