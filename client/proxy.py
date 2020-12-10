7e5bbf3e62a7e5c0982a916f75d2abfeea74b692
Ilay Levy <ilayl9991@gmail.com>
2020-12-10 13:21:40 +0200

    in progress

--- client/proxy.py
+++ client/proxy.py
@@ -1,4 +1,19 @@
 from scapy.all import *
 
-def routing(p, my_vir_ip, ips : list):
-    return p[IP].dst in ips or p[IP].dst == my_vir_ip
+my_vir_ip = "127.0.0.1"
+ips = ["127.0.0.1", "168.192.1.1", "168.192.1.5", "25.200.2.86"]
+def routing(p):
+    global my_vir_ip
+    global ips
+    if IP in p:
+        return p[IP].dst in ips or p[IP].dst == my_vir_ip
+    else:
+        return False
+
+def prnt(p):
+    print(p.summary())
+    print("send to => " + p[IP].dst)
+
+print("start:")
+sniff(lfilter=routing, prn=prnt)
+print("end")
